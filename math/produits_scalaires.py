df45f5864e1a76197e96117e5a724004c98026ea
X3noX664 <141780234+X3noX664@users.noreply.github.com>
2025-03-31 22:44:41 +0200

    Update produits_scalaires.py

--- math/produits_scalaires.py
+++ math/produits_scalaires.py
@@ -1,3 +1,4 @@
+ import math
 choix = input("""1 somme de vecteurs
 2 coordonnées d'un vecteur
 3 norme d'un vecteur
@@ -12,10 +13,36 @@ quelle est ton choix""")
 
 if choix == "1":
   x1 = int(input("coordonnée x1"))
-  y1 = int(input("coordonnée y1"))
   x2 = int(input("coordonnée x2"))
+  y1 = int(input("coordonnée y1"))
   y2 = int(input("coordonnée y2"))
   x = x1 + x2
   y = y1 + y2
-  print("""u+v = (x1+x2;y1+y2)
-  u+v = ({x} ; {y})""") 
+  print(f"""u+v = (x1+x2;y1+y2)
+  u+v = ({x} ; {y})""")
+elif choix == "2":
+  x1 = int(input("coordonnée xa"))
+  x2 = int(input("coordonnée xb"))
+  y1 = int(input("coordonnée ya"))
+  y2 = int(input("coordonnée yb"))
+  x = x2 - x1
+  y = y2 - y1
+  print(f"""AB(xb-xa;yb-ya) 
+AB = ({x};{y})""")
+elif choix == "3":
+  choix2 = input("utilise tu les coordonnées de chaque points (1) ou de chaques vecteurs (2) ?")
+  if choix2 == "1":
+     x1 = int(input("coordonnée xa"))
+     x2 = int(input("coordonnée xb"))
+     y1 = int(input("coordonnée ya"))
+     y2 = int(input("coordonnée yb"))
+     x = x2 - x1
+     y = y2 - y1
+     print(f"""AB(xb-xa;yb-ya) 
+     AB = ({x};{y})""")
+     v = x**2 + y**2
+     u = math.sqrt(v)
+     print(f"""u = √(xb-xa)²+(yb-ya)²)
+     = √({x2}-{x1})²+({y2}-{y1})²)
+     = {u}""")
+                  
